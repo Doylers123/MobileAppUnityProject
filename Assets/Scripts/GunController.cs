139cdbd39701d4bd61078f98ce97452eea3abe89
Doylers123 <g00335783@gmit.ie>
2019-04-03 18:22:14 +0100

    first commit

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+    public bool isFiring;
+
+    public BulletController bullet;
+    public float bulletSpeed;
+
+    public float timeBetweenShots;
+    private float shotCount;
+
+    public Transform firePoint;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (isFiring)
+        {
+            shotCount -= Time.deltaTime;
+            if (shotCount <= 0)
+            {
+                shotCount = timeBetweenShots;
+                BulletController newBullet = Instantiate(bullet, firePoint.position, firePoint.rotation) as BulletController;
+                newBullet.speed = bulletSpeed;
+            }
+        } else {
+            shotCount = 0;
+        }
+     }
+}
---
+++ Assets/Scripts/HurtPlayerScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtPlayerScript : MonoBehaviour {
+
+    public int damageToGive;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerHealthManager>().HurtPlayerScript(damageToGive);
+        }
+    }
+}
